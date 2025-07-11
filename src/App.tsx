254af37a15c6081a794249b397f07f9f987b18bc
Lakshay Jain <131363148+Lakshay12356@users.noreply.github.com>
2025-07-11 13:56:22 +0000

    alsk

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Testimonials from './components/Testimonials'
 import Footer from './components/Footer'
 import BookingOptions from './components/Bookings'
 import FAQ from './components/Faq'
+import { FloatingDockDemo } from './components/Dock'
 
 const App = () => {
   return (
     <div className=''>
       <Navbar />
+      <FloatingDockDemo />
       <Hero />
       <ChoosePackage />
       <Testimonials />
