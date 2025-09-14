ed90f2aa5cdc2acced5a6ba1480d769da10399ec
Lakshay Jain <131363148+Lakshay12356@users.noreply.github.com>
2025-09-14 15:30:39 +0000

    abababab

--- src/components/Packages.tsx
+++ src/components/Packages.tsx
@@ -4,7 +4,7 @@ const ChoosePackage = () => {
 
   return (
     <div id="services" className="px-6 py-12 font-[Inter] text-gray-800">
-      <h2 className="text-4xl sm:text-5xl font-bold text-center mb-20">
+      <h2 className="text-4xl sm:text-5xl font-bold text-center mb-2">
         Explore Our Offerings
       </h2>
       <div className="flex flex-wrap justify-center gap-8">
